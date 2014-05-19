7310c8c183a6f890bbe4f3a6d1ad76df12da41bf
hallolin <501125953@qq.com>
2014-05-19 22:03:34 +0800

    日历动态更新

--- src/main/webapp/resources/js/lcIndex.js
+++ src/main/webapp/resources/js/lcIndex.js
@@ -1,20 +1,21 @@
-var expand1 = false;
+
 function displayCal(id)
 {
     list="#list"+id
     calArea="#calArea"+id
     ifr="#iframe"+id
     
+       eval("expand" +id+"="+"false");
 	
 	
-		if (expand1 == false) {
+		if (eval("expand" +id) == false) {
 			$(list).animate({
 				height : "500"
 			}, 400);
 			$(calArea).css("display", "block");
 			$(ifr).attr("src", "/trms/user/calendar?room_ID="+"room");
 			expand1 = true;
-		} else if (expand1 == true) {
+		} else if (eval("expand" +id) == true) {
 			$(list).animate({
 				height : "84"
 			}, 400);
